eff06a9c796418f16a035192c75f23bf4046bc5a
jeswsi <james@woodruffscientific.com>
2020-02-04 11:47:30 -0800

    Split out staging nested stack

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -7,12 +7,12 @@ CLI_PROFILE=awsbootstrap
 EC2_INSTANCE_TYPE=t2.micro
 
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
-    --query "Account" --output text`
-
+        --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
 
 # Generate a personal access token with repo and admin:repo_hook
-# permissions from https://github.com/settings/tokens
+#    permissions from https://github.com/settings/tokens
 GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
 GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
 GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
@@ -28,18 +28,36 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-  CodePipelineBucket=$CODEPIPELINE_BUCKET
+    CodePipelineBucket=$CODEPIPELINE_BUCKET \
+    CloudFormationBucket=$CFN_BUCKET
+
+# Package up CloudFormation templates into an S3 bucket
+echo -e "\n\n=========== Packaging main.yml ==========="
+mkdir -p ./cfn_output
+
+PACKAGE_ERR="$(aws cloudformation package \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --template main.yml \
+  --s3-bucket $CFN_BUCKET \
+  --output-template-file ./cfn_output/main.yml 2>&1)"
+
+if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+  echo "ERROR while running 'aws cloudformation package' command:"
+  echo $PACKAGE_ERR
+  exit 1
+fi
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
---region $REGION \
---profile $CLI_PROFILE \
---stack-name $STACK_NAME \
---template-file main.yml \
---no-fail-on-empty-changeset \
---capabilities CAPABILITY_NAMED_IAM \
---parameter-overrides \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file ./cfn_output/main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE \
     GitHubOwner=$GH_OWNER \
     GitHubRepo=$GH_REPO \
@@ -47,9 +65,9 @@ aws cloudformation deploy \
     GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
-
+# If the deploy succeeded, show the DNS name of the endpoints
 if [ $? -eq 0 ]; then
-aws cloudformation list-exports \
---profile awsbootstrap \
---query "Exports[?starts_with(Name,'LBEndpoint')].Value"
+  aws cloudformation list-exports \
+    --profile awsbootstrap \
+    --query "Exports[?ends_with(Name,'LBEndpoint')].Value"
 fi
